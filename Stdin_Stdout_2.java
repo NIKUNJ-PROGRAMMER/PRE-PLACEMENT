b48b3cb12961f78a97de49a7d7ca4598d59b6375
Nikunj <nikunjpareek042@gmail.com>
2026-06-10 18:59:48 +0530

    Add files via upload

---
+++ Stdin_Stdout_2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        double d1=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+        
+        System.out.println("String: "+s);
+        System.out.println("Double: "+d1);
+        System.out.println("Int: "+n1);
+    }
+}
